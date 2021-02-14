4d872daec67e4373599782ff22f39fa9944bfd6a
unknown <snowpeech@gmail.com>
2021-02-14 13:33:59 -0600

    trying another db connection config

--- config.js
+++ config.js
@@ -7,23 +7,25 @@ const SECRET = process.env.SECRET_KEY || 'londonfog123';
 const PORT = +process.env.PORT || 3001;
 
 const BCRYPT_WORK_FACTOR = 12;
-// database is:
-//
-// - on Heroku, get from env var DATABASE_URL
-// - in testing, 'jobly-test'
-// - else: 'jobly'
 
 let DB_URI;
 
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "hundreddays_test";
-} else {
-  DB_URI  = process.env.DATABASE_URL || 'hundreddays';
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = "hundreddays_test";
+// } else {
+//   DB_URI  = process.env.DATABASE_URL || 'hundreddays';
+// }
+
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  return (process.env.NODE_ENV === "test")
+      ? "hundreddays_test"
+      : process.env.DATABASE_URL || "hundreddays";
 }
 
 module.exports = {
   SECRET,
   PORT,
   BCRYPT_WORK_FACTOR,
-  DB_URI,
+  getDatabaseUri,
 };
--- db.js
+++ db.js
@@ -1,17 +1,20 @@
+"use strict";
+
 const { Client } = require("pg");
+const {getDatabaseUri} = require("./config");
 
-let DB_URI;
-console.log("I'm in db.js")
-if (process.env.NODE_ENV === "test") {
-  DB_URI = "postgresql:///hundreddays_test";
-} else {
-  DB_URI = process.env.DATABASE_URL || "postgresql:///hundreddays";
-  console.log("DB_URI::", DB_URI)
-  // DB_URI = "postgresql:///hundreddays";
-}
+// let DB_URI;
+console.log("I'm in db.js");
+// if (process.env.NODE_ENV === "test") {
+//   DB_URI = "postgresql:///hundreddays_test";
+// } else {
+//   DB_URI = process.env.DATABASE_URL || "postgresql:///hundreddays";
+//   console.log("DB_URI::", DB_URI)
+//   // DB_URI = "postgresql:///hundreddays";
+// }
 
-let db = new Client({
-  connectionString: DB_URI
+const db = new Client({
+  connectionString: getDatabaseUri()
 });
 
 db.connect();
